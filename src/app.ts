ac241b5e9819e87d7a2d156bebf9354bc48175be
Abdelmounaim Ammari <abdelmounaim.ammari@gmail.com>
2024-09-16 16:06:14 +0200

    feat: Add Register page and client side form valication for both login and register forms

--- src/app.ts
+++ src/app.ts
@@ -23,7 +23,10 @@ app.get('/', (req: Request, res: Response) => {
   res.render('index', { title: 'Welcome to My Express App!' });
 });
 
-app.get('/login',(req:Request,res:Response)=>{
-  res.render('login');
+app.get('/auth/login',(req:Request,res:Response)=>{
+  res.render('auth/login');
 });
 
+app.get('/auth/register',(req:Request,res:Response)=>{
+  res.render('auth/register');
+});
