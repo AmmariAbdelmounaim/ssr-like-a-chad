f9c00e3633a80c2aea7f2f96612c88eeece99452
Abdelmounaim Ammari <abdelmounaim.ammari@gmail.com>
2024-09-25 18:14:28 +0200

    feat: Dynamic UI for agent dashboard

---
+++ src/const.ts
@@ -0,0 +1,52 @@
+export const propertyListings = [
+  {
+    title: "Charmant appartement 2 chambres au centre-ville",
+    propertyType: "Vente",
+    publicationStatus: "Publié",
+    propertyStatus: "Disponible",
+    description:
+      "Un bel appartement de 2 chambres situé au cœur de la ville, parfait pour une vie urbaine. La propriété dispose d'équipements modernes et d'un espace de vie ouvert spacieux.",
+    price: 250000,
+    availabilityDate: new Date("2024-10-15"),
+    photos: [
+      "https://t4.ftcdn.net/jpg/03/70/64/43/360_F_370644357_MDF4UXLAXTyyi2OyuK66tWW9cA2f8svL.jpg",
+      "https://example.com/images/property1.jpg",
+      "https://example.com/images/property2.jpg",
+      "https://example.com/images/property3.jpg",
+    ],
+    commentsCount: 12,
+  },
+  {
+    title: "Maison familiale avec jardin à la campagne",
+    propertyType: "Vente",
+    publicationStatus: "Publié",
+    propertyStatus: "Vendu",
+    description:
+      "Grande maison familiale avec 4 chambres, située à la campagne, offrant un grand jardin et une vue imprenable sur les environs.",
+    price: 450000,
+    availabilityDate: new Date("2024-08-01"),
+    photos: [
+      "https://cdn.lecollectionist.com/__lecollectionist__/production/houses/7194/photos/gb7CwF8US12864ThDGev_84abcf71-318a-42d5-8fac-9e6513a47b8a.jpg?width=2880&q=65",
+      "https://example.com/images/property5.jpg",
+      "https://example.com/images/property6.jpg",
+    ],
+    commentsCount: 5,
+  },
+  {
+    title: "Studio moderne en location près de la plage",
+    propertyType: "Location",
+    publicationStatus: "Publié",
+    propertyStatus: "Disponible",
+    description:
+      "Un studio entièrement meublé et moderne, à seulement 5 minutes à pied de la plage. Idéal pour des séjours de courte ou longue durée.",
+    price: 850,
+    availabilityDate: new Date("2024-09-01"),
+    photos: [
+      "https://cf.bstatic.com/xdata/images/hotel/max1024x768/295090917.jpg?k=d17621b71b0eaa0c7a37d8d8d02d33896cef75145f61e7d96d296d88375a7d39&o=&hp=1",
+      "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.booking.com%2Fhotel%2Fgr%2Felysian-villa.fr.html&psig=AOvVaw3zXstUKZs1tDVoenQllSdQ&ust=1727366104815000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCPDO4Km63ogDFQAAAAAdAAAAABAQ",
+      "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.istockphoto.com%2Ffr%2Fphotos%2Fvilla-moderne&psig=AOvVaw1zk_hZpL3C6tLQ9Quy-LCE&ust=1727366020717000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCICJuYG63ogDFQAAAAAdAAAAABAQ",
+      "https://example.com/images/property8.jpg",
+    ],
+    commentsCount: 18,
+  },
+];
